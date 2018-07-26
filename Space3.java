8d8a6ef88bc78ee7620ab92c4a568aab6b4ae3dc
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-07-26 17:26:51 +0530

    Add files via upload

---
+++ Space3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Space3 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String n=s.nextLine();
+		int count=0;
+		char ch[]=n.toCharArray();
+		for(int i=0;i<n.length();i++)
+		{
+			if(ch[i]==' ')
+				count++;
+			}
+		System.out.println(count);
+	}
+
+}
